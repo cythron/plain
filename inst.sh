85b92b37af6e106598a5d6a32e9ac18d4704ffb9
cythron <63314624+cythron@users.noreply.github.com>
2020-04-09 09:48:53 +0545

    Create inst.sh

---
+++ inst.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+while read dependency; do
+    dependency_stripped="$(echo "${dependency}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+    # Skip comments
+    if [[ $dependency_stripped == \#* ]]; then
+        continue
+    # Skip blank lines
+    elif [ -z "$dependency_stripped" ]; then
+        continue
+    else
+        if pip install "$dependency_stripped"; then
+            echo "$dependency_stripped is installed"
+        else
+            echo "Could not install $dependency_stripped, skipping"
+        fi
+    fi
+done < requirements.txt
